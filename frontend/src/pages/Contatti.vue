62b69d0f09be0a2c4873fc84f27d15902bc6a8de
Livio Scarpinati <livio.scarpinati@gmail.com>
2022-04-15 16:02:46 +0200

    chore: pulizia codice pagina contatti

--- frontend/src/pages/Contatti.vue
+++ frontend/src/pages/Contatti.vue
@@ -11,38 +11,11 @@
           <img :src="require('../assets/images/squared_logo.png')" alt="" />
         </div>
         <h3 class="title">CONTATTI</h3>
-        <!--
-        <p class="category">Photographer</p>
-        <div class="content">
-          <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
-          </div>
-          <div class="social-description">
-            <h2>26</h2>
-            <p>Comments</p>
-          </div>
-          <div class="social-description">
-            <h2>48</h2>
-            <p>Bookmarks</p>
-          </div>
-        </div>
-        -->
       </div>
     </div>
     <div class="section">
       <div class="container">
         <div class="button-container">
-          <!--
-          <a href="#button" class="btn btn-primary btn-round btn-lg">Follow</a>-->
-          <!-- <a
-            href="#button"
-            class="btn twitter-color btn-round btn-lg btn-icon"
-            rel="tooltip"
-            title="Follow me on Twitter"
-          >
-            <i class="fab fa-twitter"></i>
-          </a> -->
           <a
             href="https://www.youtube.com/watch?v=ZO0cf8fyk40"
             class="btn youtube-color btn-round btn-lg btn-icon"
@@ -69,82 +42,15 @@
           insegnanti e promuove progetti di ricerca scientifica in ambito
           geologico-ambientale e didattico.
         </h5>
-        <!-- <div class="row">
-          <div class="col-md-6 ml-auto mr-auto">
-            <h4 class="title text-center">My Portfolio</h4>
-          </div>
-          <tabs
-            pills
-            class="nav-align-center"
-            tab-content-classes="gallery"
-            tab-nav-classes="nav-pills-just-icons"
-            type="primary"
-          >
-            <tab-pane title="Profile">
-              <i slot="label" class="now-ui-icons design_image"></i>
-
-              <div class="col-md-10 ml-auto mr-auto">
-                <div class="row collections">
-                  <div class="col-md-6">
-                    <img src="img/bg6.jpg" class="img-raised" />
-                    <img src="img/bg11.jpg" alt="" class="img-raised" />
-                  </div>
-                  <div class="col-md-6">
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                  </div>
-                </div>
-              </div>
-            </tab-pane>
-
-            <tab-pane title="Home">
-              <i slot="label" class="now-ui-icons location_world"></i>
-
-              <div class="col-md-10 ml-auto mr-auto">
-                <div class="row collections">
-                  <div class="col-md-6">
-                    <img src="img/bg1.jpg" alt="" class="img-raised" />
-                    <img src="img/bg3.jpg" alt="" class="img-raised" />
-                  </div>
-                  <div class="col-md-6">
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                  </div>
-                </div>
-              </div>
-            </tab-pane>
-
-            <tab-pane title="Messages">
-              <i slot="label" class="now-ui-icons sport_user-run"></i>
-
-              <div class="col-md-10 ml-auto mr-auto">
-                <div class="row collections">
-                  <div class="col-md-6">
-                    <img src="img/bg3.jpg" alt="" class="img-raised" />
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                  </div>
-                  <div class="col-md-6">
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                    <img src="img/bg6.jpg" class="img-raised" />
-                  </div>
-                </div>
-              </div>
-            </tab-pane>
-          </tabs>
-        </div> -->
       </div>
     </div>
   </div>
 </template>
 <script>
-import { Tabs, TabPane } from "@/components";
-
 export default {
   name: "profile",
   bodyClass: "profile-page",
   components: {
-    //Tabs,
-    //TabPane,
   },
 
   data() {

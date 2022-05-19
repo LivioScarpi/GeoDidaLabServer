b59fbacc5a42258834c4db513ae397f6f9670a7b
Livio Scarpinati <livio.scarpinati@gmail.com>
2022-05-19 17:46:55 +0200

    chore: sistemato il footer

--- frontend/src/layout/MainFooter.vue
+++ frontend/src/layout/MainFooter.vue
@@ -13,8 +13,25 @@
           <li>
             <a v-on:click="goToContattiPage()" style="cursor: pointer;"> Contatti </a>
           </li>
+
+          <li>
+             <a style="cursor: pointer;" href="http://www.di.unito.it/do/home.pl">Powered by Dipartimento di Informatica - UniTo </a>
+          </li>
         </ul>
       </nav>
+      <div class="copyright">
+        &copy; {{ year }}, Designed by
+        <a href="https:///www.invisionapp.com" target="_blank" rel="noopener"
+          >Invision</a
+        >. Coded by
+        <a href="https://binarcode.com" target="_blank" rel="noopener"
+          >BinarCode</a
+        >
+        and
+        <a href="https://www.creative-tim.com" target="_blank" rel="noopener"
+          >Creative Tim</a
+        >.
+      </div>
       <!--
       <div class="copyright">
         &copy; {{ year }}, Designed by
